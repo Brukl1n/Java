2bb23fe49a6218c7153270b9e21f89fa5a71f50a
Brukl1n <enot.east@gmail.com>
2023-04-03 02:37:03 +0300

    Completed second homework

--- homework_2/task_1_2/Main.java
+++ homework_2/task_1_2/Main.java
@@ -8,22 +8,20 @@
 // Студент Краснов получил 5 по предмету Физика.
 // Используйте StringBuilder для подготовки ответа
 
-package Java.homework_2.task_1;
+package Java.homework_2.task_1_2;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
     public static String readFile(String path) {
         String line = "";
         try {
-            String str;
             File file = new File(path);
             BufferedReader bf = new BufferedReader(new FileReader(file));
-            while((str = bf.readLine()) != null) {
-                line += str + "\n";
-            }
+            line = bf.readLine();
             bf.close();
             return line;
         } catch (Exception e) {
@@ -33,23 +31,36 @@ public class Main {
 
     public static void parser(String line) {
         String[] text = new String[] {"Студент", "получил", "по предмету"};
-        String[] parseString = line.split("\n");
+        String[] parseString = line.split("},");
         for (String string : parseString) {
-            String[] studentString = string.split("(:|,|})");
-            for(int i = 0; i < studentString.length; i++) {
+            String[] studentString = string.split("(:|,|}])");
+            for (int i = 0; i < studentString.length; i++) {
                 studentString[i] = studentString[i].replace("\"", "");
             }
             StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < text.length; i++) {
+            for (int i = 0; i < text.length; i++) {
                 sb.append(text[i] + " ");
-                sb.append(studentString[i * 2 + 1] + " ");
+                sb.append(studentString[i*2 + 1] + " ");
             }
             System.out.println(sb.toString());
+            writeFile(sb.toString());
+        }
+    }
+
+    public static void writeFile(String str) {
+        String path = "Java/homework_2/task_1_2/result.txt";
+        File logFile = new File(path);
+        try {
+            FileWriter writer = new FileWriter(logFile, true);
+            writer.write(str + "\n");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Что то пошло не так");
         }
     }
 
     public static void main(String[] args) {
-        String path = "Java/homework_2/task_1/gradebook.json";
+        String path = "Java/homework_2/task_1_2/gradebook.json";
         parser(readFile(path));
     }
 }
