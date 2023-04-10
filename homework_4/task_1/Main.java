b7b5aef0c1517bd4ad4f3ce1b2d811f0c244b7e2
Brukl1n <enot.east@gmail.com>
2023-04-10 22:56:38 +0300

    Completed fourth homework

--- homework_4/task_1/Main.java
+++ homework_4/task_1/Main.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
+    
     public static void main(String[] args) {
         LinkedList<Integer> linList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
         System.out.println(reversArray(linList));
--- homework_4/task_3/Main.java
+++ homework_4/task_3/Main.java
@@ -1,5 +1,39 @@
+// Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. Используйте итератор
+
 package Java.homework_4.task_3;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
     
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        inputLinkedList(list);
+        System.out.println(list);
+        System.out.println(sumLinkedList(list));
+    }
+
+    static void inputLinkedList(LinkedList<Integer> list){
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("size: ");
+            int size = Integer.parseInt(scanner.nextLine());
+            for (int i = 0; i < size; i++) {
+                System.out.print("#" + i + ": ");
+                list.add(Integer.parseInt(scanner.nextLine()));
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int sumLinkedList(LinkedList<Integer> array){
+        int sum = 0;
+        ListIterator<Integer> iterator = array.listIterator(0);
+        while (iterator.hasNext()) {
+            sum += iterator.next();
+        }
+        return sum;
+    }
 }
